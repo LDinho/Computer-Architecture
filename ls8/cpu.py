5507ad6b913551767098e8c4bcd38df367276948
LD <LDinho@users.noreply.github.com>
2020-04-25 10:54:47 -0700

    add mvp for day 1 & 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,41 +2,84 @@
 
 import sys
 
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+MUL = 0b10100010
+
+is_running = True
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
 
-    def load(self):
+    def ram_read(self, address):
+        data = self.ram[address]
+        return data
+
+    def ram_write(self, address, data):
+        self.ram[address] = data
+
+    def load(self, filename):
         """Load a program into memory."""
 
-        address = 0
+        # address = 0
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
+        #
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        try:
+            address = 0
 
+            with open(filename) as file:  # open file
+
+                for line in file:  # read each line
+                    comment_split = line.split("#")  # remove comments
+                    number_string = comment_split[0].strip()  # remove new lines
+
+                    if number_string == "":  # ignore blank lines
+                        continue
+                    instruction_number = int(number_string, 2)
+
+                    # this just prints out the binary code and conversion to decimal
+                    print(f'{instruction_number:08b} is {instruction_number:0d}')
+
+                    self.ram[address] = instruction_number  # populate memory array
+                    address += 1
+
+        except FileNotFoundError:
+            print("no file found")
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+
+        elif op == MUL:
+            self.reg[reg_a] *= self.reg[reg_b]
+
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -62,4 +105,23 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        while is_running:  # while running is True
+
+            opcode = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            if opcode == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif opcode == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif opcode == MUL:
+                self.alu(opcode, operand_a, operand_b)
+                self.pc += 3
+            elif opcode == HLT:
+                sys.exit(0)
+            else:
+                print(f"running program failed")
+                sys.exit(2)
